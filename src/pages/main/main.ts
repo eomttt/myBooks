8791062898b329979a4df4d6740ec3e2a6731fe6
hyuntaeeom <hyuntaeeom@hyuntaeeomui-MacBookPro.local>
2019-03-22 15:53:10 +0900

    Before inifinite scroll

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 // Import providers
 import { BooksProvider } from '../../providers/books/books';
@@ -18,6 +18,7 @@ export class MainPage {
 
   constructor(public navCtrl: NavController,
               public navParams: NavParams,
+              private alertCtrl: AlertController,
               private booksPvdr: BooksProvider) {
   }
 
@@ -39,12 +40,33 @@ export class MainPage {
 
       this._setMyBooks(books);
     } catch(error) {
-
+      this._showGetBooksError(error);
     }
   }
 
   private _setMyBooks(newBooks) {
+    console.log('Set new books', newBooks);
     this.myBooks = this.myBooks.concat(newBooks);
+  }
+
+  /**
+   * Private function
+   */
+
+  private _showGetBooksError(error) {
+    let alert = this.alertCtrl.create({
+      title: '알림',
+      message: '책을 가져오는데에 실패했습니다. <br> 잠시 후 다시 시도해주세요. 반복적으로 문제가 발생할 경우 문의하기를 사용해주세요. <br>' + error.message,
+      buttons: [
+        {
+          text: '확인',
+          handler: () => {
+            // Nothing
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 
   private _active() {
--- src/providers/books/books.ts
+++ src/providers/books/books.ts
@@ -4,7 +4,6 @@ import { Platform } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 import { Crop, CropOptions } from '@ionic-native/crop';
 import { File } from '@ionic-native/file';
-import { Base64 } from '@ionic-native/base64';
 import { UniqueDeviceID } from '@ionic-native/unique-device-id';
 
 import { AngularFireStorage } from '@angular/fire/storage';
@@ -13,69 +12,14 @@ import { AngularFireDatabase } from '@angular/fire/database';
 @Injectable()
 export class BooksProvider {
 
-  mockData = [
-    {
-      id: '1',
-      title: 'Harry Potter Number one',
-      text: 'First Readed book',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/1.png?alt=media&token=4bc8dd49-d81a-4e3f-99f0-d74ef1644919'
-    },
-    {
-      id: '2',
-      title: 'Load of ring',
-      text: 'Good at me',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/2.png?alt=media&token=f868f629-1a88-4200-ad9f-65c2986c35ea'
-    },
-    {
-      id: '3',
-      title: 'Cafe homes',
-      text: 'I like americano',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/3.png?alt=media&token=12c69962-67f4-4c30-8dc3-f555992e7085'
-    },
-    {
-      id: '4',
-      title: 'Nami supermarket',
-      text: 'Good bamletter',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/4.png?alt=media&token=07180560-5e4e-4308-8e15-47876d3ad22a'
-    },
-    {
-      id: '5',
-      title: 'Young is heart',
-      text: 'Im young let',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/5.png?alt=media&token=ea0f0854-e055-4819-9b93-30d64ec73e36'
-    },
-    {
-      id: '6',
-      title: 'Good psycologist',
-      text: 'Psychologist is amazing',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/6.png?alt=media&token=35285554-9544-49ca-aa18-ff073cd4704a'
-    },
-    {
-      id: '7',
-      title: 'Black comedies',
-      text: 'I like comedies',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/7.png?alt=media&token=d3796350-b614-470e-b89b-97933da5df82'
-    },
-    {
-      id: '8',
-      title: 'Good at ballet',
-      text: 'I can do everything',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/8.png?alt=media&token=4e98782f-5467-409d-bf0a-dc286ede2085'
-    },
-    {
-      id: '9',
-      title: 'Dont sleep for history',
-      text: 'Very fun',
-      image: 'https://firebasestorage.googleapis.com/v0/b/mybookeom.appspot.com/o/9.png?alt=media&token=70419794-c99b-445b-b8a6-a85b05c4fdae'
-    },
-  ];
+  booksData = [];
+  booksId = [];
 
   constructor(private http: HttpClient,
               private platform: Platform,
               private camera: Camera,
               private crop: Crop,
               private file: File,
-              private base64: Base64,
               private uniqueDeviceID: UniqueDeviceID,
               private angularFbSt: AngularFireStorage,
               private angularFbDb: AngularFireDatabase) {
@@ -83,9 +27,55 @@ export class BooksProvider {
   }
 
   public getMyBooks() {
-    return new Promise((resolve, reject) => {
-      resolve(this.mockData)
+    return new Promise(async (resolve, reject) => {
+      try {
+        let deviceUniqueId = await this.uniqueDeviceID.get();
+        // this.angularFbDb.database(deviceUniqueId + '/books');
+        let databaseRef = this.angularFbDb.database.ref(deviceUniqueId + '/books');
+
+        let booksDataLen = this.booksData.length;
+        let data: any = {};
+        let assignedData: any = {};
+
+        if (booksDataLen > 0) {
+          data = await databaseRef.startAt(this.booksId[booksDataLen - 1]).limitToFirst(5).once('value');
+        } else {
+          data = await databaseRef.orderByKey().limitToFirst(5).once('value');
+        }
+
+        assignedData = this._setMyBooks(data.val());
+        console.log('Get books', assignedData);
+
+        this.booksData = this.booksData.concat(assignedData.values);
+
+        resolve(this.booksData);
+      } catch(error) {
+        console.log('Get my books error', error);
+      }
     });
+  }
+
+  private _setMyBooks(data) {
+    let booksValue = [],
+        booksKey = [];
+
+    let newBook = {};
+
+    Object.keys(data).map((key) => {
+      newBook = {
+        id: key
+      };
+
+      Object.assign(newBook, data[key]);
+
+      booksKey.push(key);
+      booksValue.push(newBook);
+    });
+
+    return {
+      values: booksValue,
+      keys: booksKey
+    };
   }
 
   public setBooksData(saveData) {
